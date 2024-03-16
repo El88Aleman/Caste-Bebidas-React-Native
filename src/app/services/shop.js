5d2eb4c37e9823fa20c981672ccc97cf2dbef97c
Fabi Tureo <fabianestebantureo@gmail.com>
2024-03-16 19:38:35 -0300

    fix: 🐛 add approach to recovery firebaseId and implement in product generated by importation

--- src/app/services/shop.js
+++ src/app/services/shop.js
@@ -1,27 +1,45 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const shopApi = createApi({
-  reducerPath: "shopApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl:
-      "https://caste-bebidas-react-nati-76ac6-default-rtdb.firebaseio.com",
-  }),
-  endpoints: (builder) => ({
-    getProductsByCategory: builder.query({
-      query: (category) =>
-        `/products.json?orderBy="category"&equalTo="${category}"`,
-      transformResponse: (response) => {
-        const data = Object.values(response);
-        return data;
-      },
+    reducerPath: "shopApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl:
+            "https://caste-bebidas-react-nati-76ac6-default-rtdb.firebaseio.com",
     }),
-    getCategories: builder.query({ query: () => "/categories.json" }),
-    getProduct: builder.query({ query: (id) => `/products/${id}.json` }),
-  }),
+    endpoints: (builder) => ({
+        getProductsByCategory: builder.query({
+            query: (category) =>
+                `/products.json?orderBy="category"&equalTo="${category}"`,
+            transformResponse: (response) => {
+                console.log({ response });
+                const data = Object.values(response);
+                console.log({ data });
+                // data --> [ {}, {}, {}, {}]
+
+                const result = data.map((product) => {
+                    const idsFirebase = Object.keys(response);
+                    console.log({ idsFirebase });
+
+                    idsFirebase.forEach((key) => {
+                        console.log({ key });
+                        product.idFirebase = key;
+                        console.log({ id: product.idFirebase });
+                    });
+                    return { ...product };
+                });
+
+                console.log({ result });
+
+                return result;
+            },
+        }),
+        getCategories: builder.query({ query: () => "/categories.json" }),
+        getProduct: builder.query({ query: (id) => `/products/${id}.json` }),
+    }),
 });
 
 export const {
-  useGetCategoriesQuery,
-  useGetProductsByCategoryQuery,
-  useGetProductQuery,
+    useGetCategoriesQuery,
+    useGetProductsByCategoryQuery,
+    useGetProductQuery,
 } = shopApi;
